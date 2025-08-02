708816ea0f97a3369f61d4b76d38610de0fb9cc7
ay981 <aymenabdulkerim411@gmail.com>
2025-08-03 00:24:29 +0300

    push

--- alx-project-0x06/components/layouts/Header.tsx
+++ alx-project-0x06/components/layouts/Header.tsx
@@ -1,12 +1,12 @@
-import React from "react";
 import Link from "next/link";
 import Button from "../common/Button";
 import { usePathname } from "next/navigation";
-import { useCount } from "../../context/CountContext";
+import { RootState } from "@/store/store";
+import { useSelector } from "react-redux";
 
 const Header: React.FC = () => {
-  const pathname = usePathname();
-  const { count } = useCount();
+  const pathname = usePathname()
+  const count = useSelector((state: RootState) => state.counter.value)
 
   return (
     <header className="fixed w-full bg-white shadow-md">
@@ -16,14 +16,16 @@ const Header: React.FC = () => {
         </Link>
         {/* Button Group */}
         <div className="flex gap-4">
-          {!["/counter-app"].includes(pathname) ? (
-            <>
-              <Button buttonLabel="Sign In" buttonBackgroundColor="red" />
-              <Button buttonLabel="Sign Up" buttonBackgroundColor="blue" />
-            </>
-          ) : (
-            <p className=" font-semibold text-lg">Current count : {count}</p>
-          )}
+          {
+            !["/counter-app"].includes(pathname) ? (
+              <>
+                <Button buttonLabel="Sign In" buttonBackgroundColor="red" />
+                <Button buttonLabel="Sign Up" buttonBackgroundColor="blue" />
+              </>
+            ) : (
+              <p className=" font-semibold text-lg">Current count : {count}</p>
+            )
+          }
         </div>
       </div>
     </header>
